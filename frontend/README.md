75d53894e3bc4c8c3c9470989b16d55e6b7b9bc9
zcb <zcbin@pku.edu.cn>
2020-12-14 14:10:12 +0800

    frontend

--- frontend/README.md
+++ frontend/README.md
@@ -16,3 +16,6 @@
 1. Build apache2: `https://blog.csdn.net/qq_16166591/article/details/93797976`
 2. Bash `npm run fe-build`
 3. Bash `rm -rf /var/www/html` `mv ./dist /var/www/html`
+
+## blog about <a-button>
+https://blog.csdn.net/weixin_43583693/article/details/101296700
--- frontend/src/components/Test.vue
+++ frontend/src/components/Test.vue
@@ -1,18 +1,50 @@
 <template>
     <div id='test' >
-        <a-textarea
-            placeholder='Type your message here.'
-            v-model='content'
-            :autosize='{ minRows: 2, maxRows: 8 }'
-        />
-        <a-button style='margin-top: 10px; float: right' type='primary' @click='create'>
-            创建文档
-        </a-button>
-        <a-button style='margin-top: 10px; float: left' type='primary' @click='instruct'>
-            使用说明
-        </a-button>
-        <div id='canvas' style='width:800px;height:800px;' align='center'>
-            <canvas id='bg' width=768 height=768></canvas>
+        <div id='func_area'>
+            <a-button class='button' @click='create_text'>新建文本</a-button>
+            <a-button class='button' @click='upload_text'>上传文本</a-button>
+            <a-button class='button' @click='download_text'>下载文本</a-button>
+            <a-button class='button' @click='remove_text'>删除文本</a-button>
+            <a-button class='button' @click='save_anno'>保存标注</a-button>
+            <a-button class='button' @click='upload_anno'>上传标注</a-button>
+            <a-button class='button' @click='download_anno'>下载标注</a-button>
+            <a-button class='button' @click='clear_anno'>清空标注</a-button>
+            <a-dropdown class='button' style='width:190px'>
+                <a-menu slot='overlay'>
+                    <a-menu-item key="1">1st item</a-menu-item>
+                    <a-menu-item key="2">2nd item</a-menu-item>
+                    <a-menu-item key="3">3rd item</a-menu-item>
+                </a-menu>
+                <a-button>
+                    选择文本 <a-icon type="down" />
+                </a-button>
+            </a-dropdown>
+            <a-button class='button' @click='add_entity'>增加实体</a-button>
+            <a-button class='button' @click='predict_entity'>预测实体</a-button>
+            <a-button class='button' @click='remove_entity'>删除实体</a-button>
+            <a-button class='button' @click='edit_entity'>编辑实体</a-button>
+            <a-button class='button' @click='add_relation'>增加关系</a-button>
+            <a-button class='button' @click='predict_relation'>预测关系</a-button>
+            <a-button class='button' @click='remove_relation'>删除关系</a-button>
+            <a-button class='button' @click='edit_relation'>编辑关系</a-button>
+        </div>
+        <div id='op_area'>
+            <a-textarea
+                placeholder='Type your message here.'
+                v-model='content'
+                :autosize='{ minRows: 6, maxRows: 6}'
+            />
+            <a-button class='button' @click='instruct'>
+                帮助
+            </a-button>
+            <a-button class='button' style='float: right' @click='show_anno'>
+                显示标注
+            </a-button>
+            <a-button class='button' style='float: right' @click='show_graph'>
+                显示图谱
+            </a-button>
+            <br>
+            <canvas id='show_area' />
         </div>
     </div>
 </template>
@@ -42,7 +74,7 @@ let backend_address: string = 'http://localhost:8002'
 // The main class
 @Component
 export default class Test extends Vue {
-    bg: any
+    show_area: any
     context: any
     content: string = ''
 
@@ -58,7 +90,7 @@ export default class Test extends Vue {
             .finally(() => {})
     }
 
-    create() {
+    create_text() {
         let title: any = prompt('请输入当前文本名称')
         if (title == null) {
             return
@@ -77,6 +109,42 @@ export default class Test extends Vue {
             .finally(() => {})
     }
 
+    upload_text() {}
+
+    download_text() {}
+
+    remove_text() {}
+
+    save_anno() {}
+
+    upload_anno() {}
+
+    download_anno() {}
+
+    clear_anno() {}
+
+    choose_text() {}
+
+    add_entity() {}
+
+    predict_entity() {}
+
+    remove_entity() {}
+
+    edit_entity() {}
+
+    add_relation() {}
+
+    predict_relation() {}
+
+    remove_relation() {}
+
+    edit_relation() {}
+
+    show_anno() {}
+
+    show_graph() {}
+
     mouseDown(e: any) {
         console.log(e.offsetX, e.offsetY)
     }
@@ -91,16 +159,47 @@ export default class Test extends Vue {
 
     mounted() {
         // Initialize components
-        this.bg = document.getElementById('bg') as HTMLCanvasElement
-        this.context = this.bg.getContext('2d') as CanvasRenderingContext2D
-        this.bg.onmousedown = this.mouseDown
-        this.bg.onmouseup = this.mouseUp
+        this.show_area = document.getElementById('show_area') as HTMLCanvasElement
+        this.context = this.show_area.getContext('2d') as CanvasRenderingContext2D
+        this.show_area.onmousedown = this.mouseDown
+        this.show_area.onmouseup = this.mouseUp
         window.addEventListener('keydown', this.keyDown)
+        this.show_area.width = 600
+        this.show_area.height = 210
     }
 }
 </script>
 
 <!-- Add 'scoped' attribute to limit CSS to this component only -->
 <style scoped>
-
+#func_area {
+    width: 210px;
+    height: 400px;
+    border-style: solid;
+    border-color: black;
+    border-width: 1.5px;
+    position: absolute;
+    top: 30px;
+    left: 30px;
+}
+#op_area {
+    width: 600px;
+    height: 400px;
+    border-style: solid;
+    border-color: blue;
+    border-width: 0px;
+    position: absolute;
+    top: 30px;
+    left: 260px;
+}
+#show_area {
+    margin-top: 10px;
+    border-style: solid;
+    border-color: black;
+    border-width: 1.5px;
+}
+.button {
+    margin-top: 10px;
+    margin-left: 10px;
+}
 </style>
